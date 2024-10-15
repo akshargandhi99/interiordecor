b729317b4fb3d6a03c5a97bc24d4a360fec8a312
A G <akshar.gandhi99@gmail.com>
2024-10-15 15:25:36 +0530

    forcing opengraph refresh

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,26 @@ import "./globals.css";
 import NavBar from "@/components/navbar/navbar";
 import StoreProvider from "@/store/store-context";
 
+const title =
+  "Moulding the design around the end user's needs, lifestyle, and daily activities";
+const description =
+  "Welcome to AVSDS! Specialising in Spatial Interior Design, we cater to the evolving needs of our clients and believe in an informal way of working, where we understand your desires and needs and not limit design to requirements.";
 export const metadata = {
-  title:
-    "Moulding the design around the end user's needs, lifestyle, and daily activities",
-  description:
-    "Welcome to AVSDS! Specialising in Spatial Interior Design, we cater to the evolving needs of our clients and believe in an informal way of working, where we understand your desires and needs and not limit design to requirements.",
+  title,
+  description,
+
+  openGraph: {
+    title: title,
+    description: description,
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 187,
+        height: 187,
+        alt: "",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
