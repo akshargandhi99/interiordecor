1aede7835b4afc260549661608adb41691fb7813
A G <akshar.gandhi99@gmail.com>
2024-10-20 12:29:12 +0530

    added all fallback fonts and changed landing page images where line was appearing

--- app/fonts.js
+++ app/fonts.js
@@ -3,4 +3,5 @@ import { Julius_Sans_One } from "next/font/google";
 export const juliusSansOne = Julius_Sans_One({
   weight: "400",
   subsets: ["latin"],
+  fallback: ["Helvetica", "Tahoma", "Verdana"],
 });
